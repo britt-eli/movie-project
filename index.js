75762f1581d2debb5dfaa0411c783276703bb640
Brittany Richards <brittany.m.richards3@gmail.com>
2021-07-26 10:13:08 -0500

    added getMovies and getAMovie functions

--- index.js
+++ index.js
@@ -1,48 +1,69 @@
 "use strict"
 
-fetch('https://scarlet-honeysuckle-twister.glitch.me/movies').then(response => {
+const serverURL = 'https://tulip-cloudy-kettle.glitch.me/movies';
+
+
+//get all movies
+fetch(serverURL).then(response => {
     response.json().then(movies =>{
         console.log(movies)
     });
 });
 
-const serverURL = 'https://scarlet-honeysuckle-twister.glitch.me/movies';
-
-fetch(serverURL)
+//get single movie
+const getAMovie = id => fetch(`${serverURL}/${id}`)
     .then(res => res.json())
-    .then(data => console.log(data));
+    .catch(error => console.error(error));
+
+//console.log to check if we can pull a single movie
+// getAMovie(2).then(result => console.log(result));
+
+//Add movie to database
+const addMovie = (movie) => fetch(`${serverURL}`, {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(movie)
+})
+    .then(res => res.json())
+    .then(data => {
+        console.log(`Success: created ${JSON.stringify(data)}`);
+        return data.id;
+    })
+    .catch(console.error);
 
 
+
+// fetch(serverURL)
+//     .then(res => res.json())
+//     .then(data => console.log(data));
+//
+//
 // const objToSend = {
-//  user: "eli"
+//  user: "eli",
 //     message: "really enjoyed movies project"
 // };
 //
-// const options = {
-//     method: 'POST',
-//     headers: {
-//         'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(objToSend),
-// };
+
 // fetch(url, options)
 //     .then( response => console.log(response) ) /* review was created successfully */
 //     .catch( error => console.error(error) ); /* handle errors */
-
-
-function AJAX(url, method = "GET", data) {
-    const options = {
-        method: method,
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    };
-    return fetch(url, options)
-        .then(res => res.json())
-        .then(responseData => responseData)
-        .catch(err => err)
-}
+//
+//
+// function AJAX(url, method = "GET", data) {
+//     const options = {
+//         method: method,
+//         headers: {
+//             'Content-Type': 'application/json',
+//         },
+//         body: JSON.stringify(data),
+//     };
+//     return fetch(url, options)
+//         .then(res => res.json())
+//         .then(responseData => responseData)
+//         .catch(err => err)
+// }
 // AJAX(serverURL,{title: "we built our own ajax function"})
 //     .then(function (data){
 //         console.log(data)
@@ -66,13 +87,13 @@ function AJAX(url, method = "GET", data) {
 // })
 //
 // .then(data => console.log(data))
+//
+// DELETE METHOD
+// AJAX(serverURL + "/5","DELETE")
+//     .then(data => console.log(data))
+//
+// AJAX(serverURL)
+//     .then(data => console.log(data))
 
-//DELETE METHOD
-AJAX(serverURL + "/5","DELETE")
-    .then(data => console.log(data))
-
-AJAX(serverURL)
-    .then(data => console.log(data))
-
-
+//
 
